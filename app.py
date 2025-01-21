2c2aff8e20e6bae38d507fc7de38f12e98d77bcb
Balajiyoganantham <balajiyoganantham4@gmail.com>
2025-01-21 13:02:26 +0530

    add initial push

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Hello, Azure Flask App!"
+@app.route("/")
+def hello_world():
+    return 'Hello World!'
+
+@app.route("/message")
+def message():
+    return 'This message is for you!'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=8000)
